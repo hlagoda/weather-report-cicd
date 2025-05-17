7b7d60fdd417af66a3d55018cf8f4e5f0bdd2dbf
hlagoda <lagodahubert@gmail.com>
2025-05-17 19:00:48 +0200

    vm creation and properties fixes

---
+++ destroy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+
+if [[ "$#" == 0 ]]; then
+    echo "Number of arguments must be 1 and is $#"
+    exit 1
+fi
+
+terraform -chdir=./workspace/{PROJECT_NAME} destroy -auto-approve
