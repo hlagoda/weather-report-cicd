ac5c310afae198aafbfedb5fe3ac157c7ee0de2f
hlagoda <lagodahubert@gmail.com>
2025-06-05 21:27:32 +0200

    db fixes

--- db_postgres/init.sql
+++ db_postgres/init.sql
@@ -2,6 +2,24 @@ CREATE SCHEMA IF NOT EXISTS weather;
 
 CREATE TABLE IF NOT EXISTS weather.current (
     id SERIAL PRIMARY KEY,
+    city VARCHAR(255) NOT NULL UNIQUE,
+    timezone_offset INT, 
+    temp FLOAT,
+    feels_like FLOAT,
+    temp_min FLOAT,
+    temp_max FLOAT,
+    weather_main VARCHAR(255),
+    weather_description VARCHAR(255),
+    pressure INT,
+    humidity INT,
+    wind_speed FLOAT,
+    sunrise TIMESTAMP,
+    sunset TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS weather.history (
+    id SERIAL PRIMARY KEY,
     city VARCHAR(255) NOT NULL,
     timezone_offset INT, 
     temp FLOAT,
@@ -15,8 +33,9 @@ CREATE TABLE IF NOT EXISTS weather.current (
     wind_speed FLOAT,
     sunrise TIMESTAMP,
     sunset TIMESTAMP,
-    current_time TIMESTAMP NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL
 );
+
 
 CREATE TABLE IF NOT EXISTS weather.forecast (
     id SERIAL PRIMARY KEY,
