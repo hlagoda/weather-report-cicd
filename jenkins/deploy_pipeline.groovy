70b25ffdecf0e1a2dba1015e1b53e7c9ebc7ce49
hlagoda <lagodahubert@gmail.com>
2025-06-06 18:59:02 +0200

    giga jenkins config

--- jenkins/deploy_pipeline.groovy
+++ jenkins/deploy_pipeline.groovy
@@ -6,7 +6,22 @@ pipeline{
     }
 
     stages {
-        stage('Build & Deploy') {
+        stage('Install 3rd party components'){
+            steps{
+                sh 'ansible-playbook app_execute.yml -t install_components -i {{ playbook_dir }}/workspace/inventory.yml --vault-password-file vault_pass.txt'
+            }
+        }
+        stage('Add files to {{ project.name}}-agent vm'){
+            steps{
+                sh 'ansible-playbook app_execute.yml -t add_files -i {{ playbook_dir }}/workspace/inventory.yml --vault-password-file vault_pass.txt'
+            }
+        }
+        stage('Deploy docker containers'){
+            steps{
+                sh 'ansible-playbook app_execute.yml -t deploy_docker -i {{ playbook_dir }}/workspace/inventory.yml --vault-password-file vault_pass.txt'
+            }
+        }
+        stage('Run Docker containers') {
             steps {
                 sh 'docker-compose up -d'
             }
